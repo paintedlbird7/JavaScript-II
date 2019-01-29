624d5c533832f945ada084e2674e79331f09f1b4
paintedlbird7 <rosaperezlarios@gmail.com>
2019-01-29 12:12:21 -0800

    Initial Commit

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,6 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a callback function and invoke the function to test your work. You have been provided an example 
+//of a problem and a solution to see how this works with our items array.  Study both the problem and the 
+//solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -6,6 +8,8 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   //Given this problem: 
   
+  const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -22,26 +26,79 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
+
+
+
+
+
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
+
+function getLength(arr, cb) {
+  return cb(arr.length);
+}
+
+getLength(items, function(length) {
+  console.log(length) //4
+});
+
+
+
+
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
 
+// Potential Solution:
+function lastItem(arr, cb) {
+  return cb(arr[3]);
+}
+
+lastItem(items, function(last) {
+  console.log(last) //Gum
+});
+
+
+
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+
+
+
+
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
 
+
+
+
+
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+
+
+
+
+
+
+
+
+
+
 
 /* STRETCH PROBLEM */
 
