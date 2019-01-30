a2327432173a279e95a7802dd16df35446d8d83f
paintedlbird7 <rosaperezlarios@gmail.com>
2019-01-30 11:53:04 -0800

    completed Array methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -70,26 +70,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 let fullName = [];
 runners.forEach(item => fullName.push(`${item.first_name} ${item.last_name}`));
-console.log(fullName)
-
-
-// var words = ['one', 'two', 'three', 'four'];
-// words.forEach(function(word) {
-//   console.log(word);
-//   if (word === 'two') {
-//     words.shift();
-//   }
-// });
-// one
-// two
-// four
-
-
-
-
-
-
-
+console.log(fullName);
 
 
 
@@ -99,25 +80,65 @@ console.log(fullName)
 //BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 
 let allCaps = [];
-console.log(allCaps); 
+ //allCaps = runners.map(item => item.first_name.toUpperCase());
+runners.map(item => toUpperCase.push(`${item.first_name}`));
+console.log(allCaps);
 
 
 
 // ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with 
+//large sized shirts so they can choose a different size. Return an array named largeShirts that contains 
+//information about the runners that have a shirt size of L and log the result
+
 let largeShirts = [];
+largeShirts = runners.filter((id) => { return id.shirt_size = 'L';});
 console.log(largeShirts);
 
+
+
+
+
 // ==== Challenge 4: Use .reduce() ====
-// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
+// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a 
+//ticketPriceTotal array and log the result
+
+
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce((total, company_name) => {
+    return total += company_name.donation;
+}, 0);
+
+
 console.log(ticketPriceTotal);
 
+
+
+
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential 
+//problems you could solve given the data set and the 5k fun run theme.  
+//Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// get potential shirt sales
+
+shirtSoldTotal = runners.reduce((total, shirt_size) => {
+    return total += company_name.shirt_size;
+}, 0);
+console.log(shirtSoldTotal);
 
 // Problem 2
+// get last names only
+
+let lastName = [];
+runners.forEach(item => fullName.push(`${item.last_name}`));
+console.log(lastName);
 
 // Problem 3
+
+// get x large size only
+
+let xlargeShirts = [];
+largeShirts = runners.filter((id) => { return id.shirt_size = 'XL';});
+console.log(xlargeShirts);
